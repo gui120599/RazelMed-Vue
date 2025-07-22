5c5fe55057a63bc8c0e6c7d41033002a7b40cd10
gui120599 <guilherme.newsoft@gmail.com>
2025-07-22 08:04:52 -0300

    first commit

---
+++ resources/js/layouts/auth/AuthSplitLayout.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import AppLogoIcon from '@/components/AppLogoIcon.vue';
+import AppLogoPng from '@/components/AppLogoPng.vue';
+import { Link, usePage } from '@inertiajs/vue3';
+
+const page = usePage();
+const name = page.props.name;
+const quote = page.props.quote;
+
+defineProps<{
+    title?: string;
+    description?: string;
+}>();
+</script>
+
+<template>
+    <div class="relative grid h-dvh flex-col items-center justify-center px-8 sm:px-0 lg:max-w-none lg:grid-cols-2 lg:px-0">
+        <div class="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
+            <div class="absolute inset-0 text-gray-900 bg-gradient-to-b md:bg-gradient-to-r from-blue-500 " />
+            <Link :href="route('home')" class="relative z-20 flex items-center text-lg font-medium text-gray-800">
+                <AppLogoIcon class="mr-2 size-8 fill-current " />
+                {{ name }}
+            </Link>
+            <AppLogoPng class="relative max-h-96 max-w-96 mx-auto my-auto" />
+            <div v-if="quote" class="relative z-20 mt-auto">
+                <blockquote class="space-y-2">
+                    <p class="text-lg">&ldquo;{{ quote.message }}&rdquo;</p>
+                    <footer class="text-sm text-neutral-300">{{ quote.author }}</footer>
+                </blockquote>
+            </div>
+        </div>
+        <div class="lg:p-8">
+            <div class="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+                <div class="flex flex-col space-y-2 text-center">
+                    <h1 class="text-xl font-medium tracking-tight" v-if="title">{{ title }}</h1>
+                    <p class="text-sm text-muted-foreground" v-if="description">{{ description }}</p>
+                </div>
+                <slot />
+            </div>
+        </div>
+    </div>
+</template>
