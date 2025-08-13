5ef1d5d2d7493d53f9588008bc5616ccc15b8321
gui120599 <guilherme.newsoft@gmail.com>
2025-08-12 22:56:43 -0300

    buildar

---
+++ public/build/assets/TableHeader.vue_vue_type_script_setup_true_lang-BhB-OZpW.js
@@ -0,0 +1,21 @@
+import{a as u,c as t}from"./index-BhtvmoF1.js";import{d as c,j as r,o as l,a as i,r as o,n,u as d}from"./app-BlXhyLUO.js";/**
+ * @license lucide-vue-next v0.468.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const b=u("SearchIcon",[["circle",{cx:"11",cy:"11",r:"8",key:"4ej97u"}],["path",{d:"m21 21-4.3-4.3",key:"1qie3q"}]]);/**
+ * @license lucide-vue-next v0.468.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const m=u("UserMinusIcon",[["path",{d:"M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2",key:"1yyitq"}],["circle",{cx:"9",cy:"7",r:"4",key:"nufk8"}],["line",{x1:"22",x2:"16",y1:"11",y2:"11",key:"1shjgl"}]]);/**
+ * @license lucide-vue-next v0.468.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const f=u("UserPlusIcon",[["path",{d:"M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2",key:"1yyitq"}],["circle",{cx:"9",cy:"7",r:"4",key:"nufk8"}],["line",{x1:"19",x2:"19",y1:"8",y2:"14",key:"1bvyxn"}],["line",{x1:"22",x2:"16",y1:"11",y2:"11",key:"1shjgl"}]]);/**
+ * @license lucide-vue-next v0.468.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const k=u("UsersIcon",[["path",{d:"M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2",key:"1yyitq"}],["circle",{cx:"9",cy:"7",r:"4",key:"nufk8"}],["path",{d:"M22 21v-2a4 4 0 0 0-3-3.87",key:"kshegd"}],["path",{d:"M16 3.13a4 4 0 0 1 0 7.75",key:"1da9ce"}]]),y={"data-slot":"table-container",class:"relative w-full overflow-auto"},x=c({__name:"Table",props:{class:{}},setup(e){const s=e;return(a,p)=>(l(),r("div",y,[i("table",{"data-slot":"table",class:n(d(t)("w-full caption-bottom text-sm",s.class))},[o(a.$slots,"default")],2)]))}}),v=c({__name:"TableBody",props:{class:{}},setup(e){const s=e;return(a,p)=>(l(),r("tbody",{"data-slot":"table-body",class:n(d(t)("[&_tr:last-child]:border-0",s.class))},[o(a.$slots,"default")],2))}}),$=c({__name:"TableCell",props:{class:{}},setup(e){const s=e;return(a,p)=>(l(),r("td",{"data-slot":"table-cell",class:n(d(t)("p-2 align-middle whitespace-nowrap [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px]",s.class))},[o(a.$slots,"default")],2))}}),g=c({__name:"TableRow",props:{class:{}},setup(e){const s=e;return(a,p)=>(l(),r("tr",{"data-slot":"table-row",class:n(d(t)("hover:bg-muted/50 data-[state=selected]:bg-muted border-b transition-colors",s.class))},[o(a.$slots,"default")],2))}}),w=c({__name:"TableHead",props:{class:{}},setup(e){const s=e;return(a,p)=>(l(),r("th",{"data-slot":"table-head",class:n(d(t)("text-muted-foreground h-10 px-2 text-left align-middle font-medium whitespace-nowrap [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px]",s.class))},[o(a.$slots,"default")],2))}}),M=c({__name:"TableHeader",props:{class:{}},setup(e){const s=e;return(a,p)=>(l(),r("thead",{"data-slot":"table-header",class:n(d(t)("[&_tr]:border-b",s.class))},[o(a.$slots,"default")],2))}});export{b as S,k as U,x as _,M as a,g as b,w as c,v as d,$ as e,f,m as g};
